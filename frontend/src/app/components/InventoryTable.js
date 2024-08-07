8650ec6f73946a28dfdea3951b597d050e497e20
Santiago León <sleoncamacho@outlook.com>
2024-08-06 23:37:21 -0500

    Fixed some table issues

--- frontend/src/app/components/InventoryTable.js
+++ frontend/src/app/components/InventoryTable.js
@@ -1,14 +1,28 @@
-'use client';
-
 import React from 'react';
 import DataTable from 'react-data-table-component';
 
 const InventoryTable = ({ data }) => {
   const columns = [
-    { name: 'ID', selector: row => row.id, sortable: true },
-    { name: 'Product Name', selector: row => row.product_name, sortable: true },
-    { name: 'Quantity', selector: row => row.quantity, sortable: true },
-    { name: 'Price', selector: row => row.price, sortable: true }
+    {
+      name: 'ID',
+      selector: row => row.id,
+      sortable: true,
+    },
+    {
+      name: 'Product Name',
+      selector: row => row.product_name,
+      sortable: true,
+    },
+    {
+      name: 'Quantity',
+      selector: row => row.quantity,
+      sortable: true,
+    },
+    {
+      name: 'Price',
+      selector: row => row.price,
+      sortable: true,
+    },
   ];
 
   return <DataTable columns={columns} data={data} pagination />;
--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import InventoryTable from '../components/InventoryTable';
 import TabsInventory from '../components/TabsInventory';
+import Image from 'next/image';
 
 const Dashboard = () => {
   const [userName, setUserName] = useState('');
@@ -35,7 +36,7 @@ const Dashboard = () => {
       };
       fetchUserData();
     }
-  }, []);
+  }, [router]);
 
   useEffect(() => {
     if (userID) {
@@ -68,6 +69,16 @@ const Dashboard = () => {
       <div className="text-center mb-8">
         <h1 className="text-3xl font-bold mb-4 text-black">Dashboard</h1>
         <p className="text-lg text-gray-700 mb-4">Welcome, {userName}</p>
+        <div className="flex items-center justify-center">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+            src="/person-hand-wave.jpg"
+            alt="Persona saludando"
+            width={180}
+            height={40}
+            priority
+          />
+        </div>
         <button onClick={handleLogout} className="px-4 py-2 bg-red-500 text-white rounded-full shadow-lg hover:bg-red-700">Logout</button>
         <p className="text-lg text-gray-700 mb-4">Here you can see data, perform CRUD operations and chat with other users.</p>
         <div className="flex items-center justify-center space-x-4 mb-4">
@@ -94,3 +105,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
